e58fa70e1bdb4ca23084325d45bc4c15bc689b0f
AntisepticAlcohol <66309025+AntisepticAlcohol@users.noreply.github.com>
2022-03-09 17:28:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[17]:
-
 
 from flask import Flask, request, render_template
 
-
-# In[18]:
-
-
 app = Flask(__name__)
-
-
-# In[19]:
-
 
 from textblob import TextBlob
 
@@ -27,9 +14,6 @@ def index():
         return(render_template("index.html", result = r))
     else:
         return(render_template("index.html", result = "2"))
-
-
-# In[20]:
 
 
 if __name__ == "__main__":
